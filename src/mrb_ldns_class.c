49354f8aaf4b5776cd0df5d4204fc98be5f673a2
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-01-14 11:32:31 +0900

    Add Resolv::IPv{4|6} class

--- src/mrb_ldns_class.c
+++ src/mrb_ldns_class.c
@@ -16,9 +16,24 @@ static void mrb_resolv_host_define(mrb_state *mrb)
     host = mrb_define_class(mrb,"Host", resolv);
 }
 
+static void mrb_resolv_ipv4_define(mrb_state *mrb)
+{
+    struct RClass *resolv, *ipv4;
+    resolv = mrb_class_get(mrb, "Resolv");
+    host = mrb_define_class(mrb, "IPv4", resolv);
+}
+
+static void mrb_resolv_ipv6_define(mrb_state *mrb)
+{
+    struct RClass *resolv, *ipv6;
+    resolv = mrb_class_get(mrb, "Resolv");
+    host = mrb_define_class(mrb, "IPv6", resolv);
+}
 
 void mrb_resolv_classes(mrb_state *mrb) 
 {
     mrb_resolv_dns_define(mrb);
     mrb_resolv_host_define(mrb);
+    mrb_resolv_ipv4_define(mrb);
+    mrb_resolv_ipv6_define(mrb);
 }

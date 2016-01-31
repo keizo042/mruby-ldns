adb422f1609159daa1c8ef33895ac49583b4e2f8
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-02-01 01:17:03 +0900

    Add Error class

---
+++ src/mrb_ldns_error.c
@@ -0,0 +1,9 @@
+#include "mrb_ldns_error.h"
+
+static void mrb_define_resolv_(mrb_state *mrb)
+{
+}
+
+void mrb_define_resolv_error_classes(mrb_state *mrb)
+{
+}
---
+++ src/mrb_ldns_error.h
@@ -0,0 +1,8 @@
+#ifndef MRB_LDNS_ERROR_H
+#define MRB_LDNS_ERROR_H
+
+#include "mruby.h"
+#include "ldns/ldns.h"
+
+void mrb_define_resolv_error_classes(mrb_state *mrb);
+#endif

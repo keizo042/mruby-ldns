8f2a1e49b629022eff01b34f4610b21b0f74a7de
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-02-01 01:31:52 +0900

    Replace address() to @address

--- src/mrb_ldns_ip.c
+++ src/mrb_ldns_ip.c
@@ -1,5 +1,21 @@
 #include "mruby.h"
 
+
+static mrb_value mrb_resolv_ipv4_init(mrb_state *mrb, mrb_value self)
+{
+    mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, "address"), mrb_str_new_cstr(mrb,""));
+    return self;
+}
+
+static mrb_value mrb_resolv_ipv4_to_name(mrb_state *mrb, mrb_value self)
+{
+    return mrb_nil_value();
+}
+
+static mrb_valeu mrb_resolv_ipv4_to_s(mrb_state *mrb, mrb_value self)
+{
+    return mrb_str_new_cstr(mrb, "");
+}
 static void mrb_define_resolv_ipv4(mrb_state *mrb)
 {
     struct RClass *resolv, *ipv4;
@@ -8,14 +24,19 @@ static void mrb_define_resolv_ipv4(mrb_state *mrb)
     
     mrb_define_class_method(mrb, ipv4, "create", NULL, MRB_ARGS_NONE() );
 
-    mrb_define_method(mrb, ipv4, "initalize", NULL, MRB_ARGS_REQ(1) );
-    mrb_define_method(mrb, ipv4, "address", NULL, MRB_ARGS_NONE() );
-    mrb_define_method(mrb, ipv4, "to_name", NULL, MRB_ARGS_NONE() );
-    mrb_define_method(mrb, ipv4, "to_s", NULL, MRB_ARGS_NONE() );
+    mrb_define_method(mrb, ipv4, "initalize", mrb_resolv_ipv4_init , MRB_ARGS_REQ(1) );
+    // mrb_define_method(mrb, ipv4, "address", NULL, MRB_ARGS_NONE() );
+    mrb_define_method(mrb, ipv4, "to_name", mrb_resolv_ipv4_to_name, MRB_ARGS_NONE() );
+    mrb_define_method(mrb, ipv4, "to_s", mrb_resolv_ipv4_to_s, MRB_ARGS_NONE() );
 
     // mrb_define_const(mrb, ipv4, "Regex", NULL);
 }
 
+static mrb_value mrb_resolv_ipv6_init(mrb_state *mrb, mrb_value self)
+{
+    mrb_iv_set(mrb, self, mrb_intern_cstr(mrb, "address"), mrb_str_new_cstr(mrb,""));
+    return self;
+}
 static void mrb_define_resolv_ipv6(mrb_state *mrb)
 {
     struct RClass *resolv, *ipv6;
@@ -24,8 +45,8 @@ static void mrb_define_resolv_ipv6(mrb_state *mrb)
     
     mrb_define_class_method(mrb, ipv6, "create", NULL, MRB_ARGS_NONE() );
 
-    mrb_define_method(mrb, ipv6, "initalize", NULL, MRB_ARGS_REQ(1) );
-    mrb_define_method(mrb, ipv6, "address", NULL, MRB_ARGS_NONE() );
+    mrb_define_method(mrb, ipv6, "initalize", mrb_resolv_ipv6_init, MRB_ARGS_REQ(1) );
+    //mrb_define_method(mrb, ipv6, "address", NULL, MRB_ARGS_NONE() );
     mrb_define_method(mrb, ipv6, "to_name", NULL, MRB_ARGS_NONE() );
     mrb_define_method(mrb, ipv6, "to_s", NULL, MRB_ARGS_NONE() );
 }

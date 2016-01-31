93e0be55c829e6d6ca5839dfb45e164b62f0c967
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-02-01 01:10:39 +0900

    WIP. IPvX class

--- src/mrb_ldns_ip.c
+++ src/mrb_ldns_ip.c
@@ -1,22 +1,38 @@
+#include "mruby.h"
 
-static void mrb_resolv_ipv4_define(mrb_state *mrb)
+static void mrb_define_resolv_ipv4(mrb_state *mrb)
 {
     struct RClass *resolv, *ipv4;
     resolv = mrb_class_get(mrb, "Resolv");
-    host = mrb_define_class(mrb, "IPv4", resolv);
+    ipv4 = mrb_define_class(mrb, "IPv4", resolv);
+    
+    mrb_define_class_method(mrb, ipv4, "create", NULL, MRB_ARGS_NONE() );
+
+    mrb_define_method(mrb, ipv4, "initalize", NULL, MRB_ARGS_REQ(1) );
+    mrb_define_method(mrb, ipv4, "address", NULL, MRB_ARGS_NONE() );
+    mrb_define_method(mrb, ipv4, "to_name", NULL, MRB_ARGS_NONE() );
+    mrb_define_method(mrb, ipv4, "to_s", NULL, MRB_ARGS_NONE() );
+
+    // mrb_define_const(mrb, ipv4, "Regex", NULL);
 }
 
-static void mrb_resolv_ipv6_define(mrb_state *mrb)
+static void mrb_define_resolv_ipv6(mrb_state *mrb)
 {
     struct RClass *resolv, *ipv6;
     resolv = mrb_class_get(mrb, "Resolv");
-    host = mrb_define_class(mrb, "IPv6", resolv);
+    ipv6 = mrb_define_class(mrb, "IPv6", resolv);
+    
+    mrb_define_class_method(mrb, ipv6, "create", NULL, MRB_ARGS_NONE() );
+
+    mrb_define_method(mrb, ipv6, "initalize", NULL, MRB_ARGS_REQ(1) );
+    mrb_define_method(mrb, ipv6, "address", NULL, MRB_ARGS_NONE() );
+    mrb_define_method(mrb, ipv6, "to_name", NULL, MRB_ARGS_NONE() );
+    mrb_define_method(mrb, ipv6, "to_s", NULL, MRB_ARGS_NONE() );
 }
 
 
 void mrb_resolv_ip_define(mrb_state *mrb)
 {
-    mrb_resolv_ipv4_define(mrb);
-    mrb_resolv_ipv6_define(mrb);
-}
+    mrb_define_resolv_ipv4(mrb);
+    mrb_define_resolv_ipv6(mrb);
 }

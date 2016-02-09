478a9a96e45de138caaa5d73bfc1f980313ea717
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-02-09 23:31:00 +0900

    Impl reverse addr

--- src/mrb_ldns_common.c
+++ src/mrb_ldns_common.c
@@ -38,42 +38,64 @@ ldns_rr_list *mrb_getaddress_rr_list(mrb_state *mrb, ldns_resolver *resolver, ch
     }
 
     records =ldns_pkt_rr_list_by_type(pkt, LDNS_RR_TYPE_A, LDNS_SECTION_ANSWER);
+    ldns_pkt_free(pkt);
+    ldns_rdf_deep_free(domain);
     if(!records)
     {
-        ldns_pkt_free(pkt);
-        ldns_rdf_deep_free(domain);
         return NULL;
     }
     return  records;
 
 }
 
-static void name_reverse(char *str)
+static char* reverse_addr(const char *src)
 {
-    char *tmp = (char *)malloc(sizeof(str) / sizeof(char));
-    int i, j;
-    int count,
-        before;
-    before =0;
-    count = 0;
-    for(i =0; i < strlen(tmp); i++)
+    char *res = NULL,
+         *str = NULL;
+    char **arr = NULL;
+    int i=0,
+        c=0,
+        len=0;
+
+    str = (char *)malloc( (strlen(src) + 1) * sizeof(char));
+    strncpy(str, src, strlen(src) + 1);
+
+    for(i=0; i < strlen(str); i++)
     {
-        if( tmp[i] == '.' ){
-
-            for(j = 0; j < count;j++)
-            {
-                str[before + j] = tmp[j];
-
-            }
-            tmp[j+1] = '.';
-            before = before + count + 1;
-            count = 0;
-
-        }else
+        if(str[i] == '.')
         {
-            count++;
+            len++;
         }
     }
+    len++;
+    arr = (char **)malloc((len + 1) * sizeof(char*));
+    arr[len] = NULL;
+
+    c =0;
+    arr[c] = strtok(str,".");
+    if( *arr == NULL)
+    {
+        return NULL;
+    }
+    c++;
+
+    while((res = strtok(NULL,".")) != NULL)
+    {
+        arr[c] = res;
+        c++;
+    }
+    res = (char *)malloc( (strlen(str) + 1) * sizeof(char) );
+    *res = NULL;
+
+    c--;
+    for(; c >0; c--)
+    {
+    strcat(res, arr[c]);
+    strcat(res,".");
+    }
+    strcat(res, arr[c]);
+
+    return res;
 }
 
 ldns_rr_list *mrb_getname_rr_list(mrb_state *mrb, ldns_resolver *resolver,char *addr)
@@ -91,9 +113,11 @@ ldns_rr_list *mrb_getname_rr_list(mrb_state *mrb, ldns_resolver *resolver,char *
     }
 
 
-    name_reverse(addr);
 
-    rev = (char *)mrb_malloc(mrb, sizeof(addr) + sizeof(arpa) + 2); 
+    rev = (char *)malloc( sizeof(addr) + sizeof(arpa) + 2); 
+    puts(reverse_addr(addr));
+    sprintf(rev, "%s.%s",reverse_addr(addr), arpa);
+    puts(rev);
     domain = ldns_dname_new_frm_str(rev); 
 
 
@@ -114,9 +138,9 @@ ldns_rr_list *mrb_getname_rr_list(mrb_state *mrb, ldns_resolver *resolver,char *
     }
 
     records = ldns_pkt_rr_list_by_type(pkt, LDNS_RR_TYPE_PTR, LDNS_RR_CLASS_IN);
+    ldns_pkt_free(pkt);
     if(!records)
     {
-        ldns_pkt_free(pkt);
         return NULL;
     }
 
